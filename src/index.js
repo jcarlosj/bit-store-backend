83ad2d60794e1af74a028f66857ed55dfc8c858b
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-05-23 12:18:01 -0500

    [+] index.js: Diferencia entre servir paginas web y hacer uso de una API

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ const app = express();
 
 /** Definir nuestras rutas o endpoints */
 app.get( '/', function( req, res ) {        // ---> http://localhost:3000/
-    res.send( 'Bienvenido a tu tienda virtual' );
+    const template = `
+        <h1>Home page</h1>
+        <p>Bienvido a nuestra tienda virtual. Disfruta de todos nuestros descuentos</p>
+        <p>Uno de nuestros mejores aliados es <a href="https://amazon.com">Amazon</a></p>
+    `;
+
+    res.send( template );
 } );
-app.get( '/saludo', function( req, res ) {            // ---> http://localhost:3000/saludo
-    res.send( 'Hola este es un saludo' );
+app.get( '/api/products', function( req, res ) {            // ---> http://localhost:3000/saludo
+    const products = [
+        { name: 'Orange', description: 'It is a fruit', price: 4, stock: 10 },
+        { name: 'Apple', description: 'It is a fruit', price: 6, stock: 0 },
+        { name: 'Pineapple', description: 'It is a fruit', price: 12, stock: 3 }
+    ];
+
+    res.json( products );
 } );        
 
 /** Lanza un servidor web usando Node/Express -> http://localhost:3000 */
