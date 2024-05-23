8ca766619657bd51a449f4ea48e17e36e274f72a
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-05-23 11:30:53 -0500

    [+] index.js: Define rutas de acceso de la aplicacion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 const express = require( 'express' );
 const app = express();
 
-/** Lanza un servidor web usando Node/Express */
+/** Definir nuestras rutas o endpoints */
+app.get( '/', function( req, res ) {        // ---> http://localhost:3000/
+    res.send( 'Bienvenido a tu tienda virtual' );
+} );
+app.get( '/saludo', function( req, res ) {            // ---> http://localhost:3000/saludo
+    res.send( 'Hola este es un saludo' );
+} );        
+
+/** Lanza un servidor web usando Node/Express -> http://localhost:3000 */
 app.listen( 3000, function() {
     console.log( 'Server running on 3000' );
 });
