9035785871a8bcacb6b05540abe9b3a4f508ffac
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-05-23 10:40:13 -0500

    [+] NPM: npm i express & Implementa express para lanzar un servidor web con node/express en el puerto 3000

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log( 'Bienvenido nuestra Ecommerce' );
+const express = require( 'express' );
+const app = express();
+
+/** Lanza un servidor web usando Node/Express */
+app.listen( 3000, function() {
+    console.log( 'Server running on 3000' );
+});
