6ee201474bc99ce3836d266ca0b6c8dbaf72468b
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-05-27 12:35:15 -0500

    [+] products.controller: Se crea el archivo del controlador para desplegar objetos JSON de productos

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,15 @@
+function getProducts( req, res ) {            // ---> http://localhost:3000/api/products/
+    const products = [
+        { name: 'Orange', description: 'It is a fruit', price: 4, stock: 10 },
+        { name: 'Apple', description: 'It is a fruit', price: 6, stock: 0 },
+        { name: 'Pineapple', description: 'It is a fruit', price: 12, stock: 3 },
+        // { name: 'Lemon', description: 'It is a fruit', price: 1, stock: 300 }
+    ];
+
+    res.json( products );
+}
+
+
+module.exports = {
+    getProducts
+};
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,17 +1,9 @@
 const { Router } = require( 'express' );
+const { getProducts } = require('../controllers/products.controller');
 const router = Router();
 
 /** Definir todas las rutas de la API para la data de productos */
-router.get( '/', function( req, res ) {            // ---> http://localhost:3000/api/products/
-    const products = [
-        { name: 'Orange', description: 'It is a fruit', price: 4, stock: 10 },
-        { name: 'Apple', description: 'It is a fruit', price: 6, stock: 0 },
-        { name: 'Pineapple', description: 'It is a fruit', price: 12, stock: 3 },
-        { name: 'Lemon', description: 'It is a fruit', price: 1, stock: 300 }
-    ];
-
-    res.json( products );
-} );
+router.get( '/', getProducts );
 
 
 module.exports = router;
