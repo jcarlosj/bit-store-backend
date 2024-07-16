5656effae78ab8bf21a92f55d094d5c6e95a94df
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-07-16 10:41:14 -0500

    [+] Product.js: Define Modelo (propiedades de la Entidad Producto) usando Mongoose

---
+++ src/models/Products.js
@@ -0,0 +1,43 @@
+const mongoose = require( 'mongoose' );
+
+/** Definir la estructura de datos (nuestro documento) */
+const ProductSchema = new mongoose.Schema({
+    // Definir propiedades, atributos o campos (Documento)
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    quantity: {
+        type: Number,
+        default: 1,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        default: 'non-category'
+    },
+    urlImage: {
+        type: String
+    },
+    state: {
+        type: Boolean,
+        default: true
+    }
+});
+
+/** Crear el modelo de datos a partir de la estructura de datos */
+const ProductModel = mongoose.model(
+    'Products',     // Nombre de la coleccion a la que vamos a asociar nuestra estructura de datos
+    ProductSchema   // Estructura de datos para los documentos de esta coleccion
+);
+
+
+module.exports = ProductModel;
