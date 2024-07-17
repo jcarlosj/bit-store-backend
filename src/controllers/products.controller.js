80722fb1fa24064f2d33ab3192f5a5324888e6a3
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-07-17 13:01:38 -0500

    [+] products.service.js: ODM Mongoose Obtiene y Elimina producto por ID [/] products.controller.js: Separa la responsabilidad de consulta a base de datos

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,22 +1,48 @@
 const ProductModel = require("../models/Products");
 
-const { dbCreateProduct } = require("../services/products.service");
+const { dbCreateProduct, dbGetProducts, dbGetProductById } = require("../services/products.service");
 
-
+// Obtener todos los productos
 async function getProducts( req, res ) {            // ---> http://localhost:3000/api/products/
-    const data = await ProductModel.find();
+    
+    try {
+        const data = await dbGetProducts();
 
-    res.json({
-        ok: true,
-        data: data
-    });
+        // Enviando un objeto JSON como respuesta al usuario que realizo la peticion
+        res.json({
+            ok: true,
+            data: data
+        });
+    } 
+    catch ( error ) {       // Capturando la excepcion
+        console.error( error );     // Mostramos el mensaje de error al desarrollador en la terminal
+        res.json({                  // Enviando un objeto JSON como respuesta al usuario que realizo la peticion
+            ok: false,
+            msg: 'Error al obtener todos los productos'
+        });   
+    }
+
 }
 
-function getProductById( req, res ) {
-    res.json({
-        ok: true,
-        msg: 'Obtiene un producto por ID'
-    });
+async function getProductById( req, res ) {
+    const productId = req.params.id;
+
+    try {
+        const data = await dbGetProductById( productId );
+
+        res.json({
+            ok: true,
+            data: data
+        });
+    } 
+    catch ( error ) {
+        console.error( error );
+        res.json({
+            ok: false,
+            msg: 'Error al obtener un producto por ID'
+        });
+    }
+
 }
 
 // Crear un nuevo producto
@@ -51,10 +77,17 @@ function updateProductById( req, res ) {
     });
 }
 
-function removeProductById( req, res ) {
+async function removeProductById( req, res ) {
+    const productId = req.params.id;
+
+    // const data = await ProductModel.findOneAndDelete({ _id: productId });
+    const data = await ProductModel.findByIdAndDelete( productId );
+
     res.json({
         ok: true,
-        msg: 'Elimina el producto por ID'
+        msg: 'Elimina el producto por ID',
+        id: productId,
+        data: data
     });
 }
 
--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,10 +1,24 @@
 const ProductModel = require("../models/Products");
 
+
 async function dbCreateProduct ( newProduct ) {
     // Insertando datos en la base de datos usando el Modelo Diseñado para esta Entidad
     return await ProductModel.create( newProduct );        
 }
 
+async function dbGetProducts () {
+    // Obteniendo todos los datos de la base de datos usando el Modelo Diseñado para esta Entidad
+    return await ProductModel.find();
+}
+
+async function dbGetProductById( id ) {
+    // Obteniendo un producto por ID de la base de datos usando el Modelo Diseñado para esta Entidad
+    return await ProductModel.findOne({ _id: id });
+}
+
+
 module.exports = {
-    dbCreateProduct
+    dbCreateProduct,
+    dbGetProducts,
+    dbGetProductById
 }
