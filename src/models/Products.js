ec91d953bab453984875da09b848f784c9777913
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-08-01 14:46:29 -0500

    [+] Product.js Opcion de squema 'versionKey' & Middleware pre, incremento __v al actualizar documento

--- src/models/Products.js
+++ src/models/Products.js
@@ -39,6 +39,61 @@ const ProductSchema = new mongoose.Schema({
     timestamps: true
 });
 
+
+ProductSchema.pre( [ 'findOneAndUpdate', 'findByIdAndUpdate' ], function() {
+    
+    /** Obtiene el objeto de actualizacion que se aplicara al documento en BD */
+    const update = this.getUpdate();
+
+    /** Valida si el objeto de actualizacion contiene el propiedad __v */
+    if (update.__v != null) {
+        delete update.__v;      // Elimina la propiedad __v
+    }
+
+    /** Definiendo las operaciones de actualizacion que deben ser revisados en la consulta */
+    const keys = ['$set', '$setOnInsert'];
+    
+    /** Iterar cada una de las propiedades el objeto de consulta  */
+    for (const key of keys) {
+
+        /** Valida si la operacion de actualizacion ($set, $setOnInsert) existen en la consulta y contiene __v */
+        if (update[key] != null && update[key].__v != null) {
+        
+            delete update[key].__v;         // Elimina la propiedad __v
+
+            /** Valida si despues de eliminar __v, el campo esta vacio */
+            if ( Object.keys(update[key]).length === 0 ) {
+                delete update[key];         // Elimina la propiedad respectica
+            }
+        }
+    }
+
+    update.$inc = update.$inc || {};        // Inicializa $inc si no existe
+    update.$inc.__v = 1;                    // Incrementar la version del documento en 1 en la propiedad __v
+});
+
+ProductSchema.pre( 'findOneAndReplace', async function() {
+    
+    // Obtener el documento actual antes de reemplazarlo
+    const doc = await this.model.findById(this.getQuery()._id);
+    if (!doc) return;
+
+    // Obtener el nuevo documento que reemplazará al antiguo
+    const replacement = this.getUpdate();
+
+    // Asegurarse de que `__v` no esté presente en el documento de reemplazo
+    if (replacement.__v != null) {
+        delete replacement.__v;
+    }
+
+    // Incrementar la versión del documento
+    replacement.__v = doc.__v + 1;
+    
+    // Actualizar el objeto de reemplazo con la nueva versión
+    this.setUpdate(replacement);
+});
+
+
 /** Crear el modelo de datos a partir de la estructura de datos */
 const ProductModel = mongoose.model(
     'Products',     // Nombre de la coleccion a la que vamos a asociar nuestra estructura de datos
