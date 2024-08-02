f2648a5da20f204d86f59a772ffbd8edb4fa3baa
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-08-02 14:38:43 -0500

    [*] Filtra mensajes de error por nombre del campo que los produce

--- src/helpers/handleResponses.js
+++ src/helpers/handleResponses.js
@@ -14,14 +14,32 @@ const handleResponseError = ( res, statusCode, msg, error ) => {
         console.error( error );                // Mostramos el mensaje de error al desarrollador en la terminal
     }
     
+    const errors = verifyFieldErrors( error );
 
     res.status( statusCode ).json({            // Enviando un objeto JSON como respuesta al usuario que realizo la peticion
         ok: false,
         msg: msg,
-        errors: 'Aqui van los errores de los campos'
+        errors: errors
     }); 
 }
  
+const verifyFieldErrors = ( error ) => {
+    const errors = {};      // Define objeto donde se almacenaran los mensajes de error y el nombre del campo que produce el error
+
+    /** Validamos si existen los errores de validacion */
+    if( error.name === 'ValidationError' ) {
+
+        /** Iteramos el objeto de error.errors por cada una de las propiedades que contenga */
+        for( let property in error.errors ) {
+            // console.log( '--->>>', property );
+            errors[ property ] = error.errors[ property ].message; 
+        }
+
+        // console.errors( errors );
+    }
+
+    return errors;
+}
 
 module.exports = {
     handleResponseSuccess,
