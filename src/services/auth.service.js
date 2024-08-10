7ba03c80e2c79375483060cb3cd833fe7d3fe106
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-08-10 13:50:40 -0500

    [+] auth.service.js: Encriptar la contraseña antes de registrar el usuario en la DB

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,5 @@
-const UserModel = require("../models/User")
+const { genSaltSync, hashSync } = require( 'bcrypt' );
+const UserModel = require("../models/User");
 
 const dbGetUserByUsername = async ( email ) => {
 
@@ -7,7 +8,24 @@ const dbGetUserByUsername = async ( email ) => {
 
 const registerUser = async ( newUser ) => {
 
-    return await UserModel.create( newUser );
+    console.log( 'Data Pura: ', newUser );
+
+    // Paso 1: Creamos un objecto (dbUser) de la clase UserModel y le pasamos la data pura
+    const dbUser = new UserModel( newUser );
+    console.log( 'Data procesada por la clase UserModel: ', dbUser );
+
+    // Paso 2: Encriptar la contraseña
+    const salt = genSaltSync();         // Generando una cadena aleatoria para combinar
+    console.log( 'salt: ', salt );
+
+    const hashPassword = hashSync( newUser.password, salt );    // Combinando la cadena aleatoria con el password del nuevo usuario
+    console.log( 'hashPassword: ', hashPassword );
+
+    dbUser.password = hashPassword;        // Actualizando la propiedad 'password' en el objeto (dbUser)
+    console.log( 'Data procesada por la clase UserModel: ', dbUser );
+
+    // Paso 3: Guarda en la Base de datos y retorna los datos del documento registrado
+    return await dbUser.save();           
 }
 
 
