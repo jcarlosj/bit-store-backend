49dcf7dc28c3c9116cdab1a5fab2012a31e088a9
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2024-08-12 16:21:13 -0500

    [+] auth.controller.js: Autenticacion de usuario PASOS 1 al 5, generando y enviando un Token al cliente

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,6 @@
+const { compareSync } = require( 'bcrypt' );
+const { sign } = require( 'jsonwebtoken' );
+
 const { handleResponseSuccess, handleResponseError } = require("../helpers/handleResponses");
 const { dbGetUserByUsername, registerUser } = require("../services/auth.service");
 
@@ -28,10 +31,52 @@ const register = async ( req, res ) => {
     
 }
 
-const login = ( req, res ) => {
-    res.json({
+const login = async ( req, res ) => {
+    // Paso 1: Obtener los datos para autenticar el usuario (username, password)
+    const inputData = req.body;
+
+    // Paso 2: Verificar si el usuario existe DB ---> email
+    const userFound = await dbGetUserByUsername( inputData.username );
+
+    if( ! userFound ) {
+        res.status( 404 ).json({
+            ok: false,
+            msg: 'El usuario no esta registrado. Por favor registrese!'
+        });
+    } 
+
+    // Paso 3: Confirmar si la contraseña es correcta 
+    const isValidPassword = compareSync(
+        inputData.password,     // Password sin encriptar de la data pura obtendida
+        userFound.password      // Password encriptado que viene de la BD
+    );
+
+    if( ! isValidPassword ) {
+        return res.status( 404 ).json({
+            ok: false,
+            msg: 'Contraseña invalida'
+        });
+    }
+    
+    // Paso 4: Generar una autenticación pasiva (TOKEN)
+    const payload = {
+        username: userFound.username,
+        name: userFound.name,
+        role: userFound.role
+    };
+
+    const token = sign(
+        payload,                    // Payload (Carga Util)
+        '78ih89gn#t6tr7grt97@',     // PALABRA-CLAVE (Semilla)
+        { expiresIn: '1h' }         // Configuracion (expiracion del token)
+    );
+
+
+    // Paso 5: Responder al cliente enviandole el Token
+    res.status( 200 ).json({
         ok: true,
-        msg: 'Autentica un usuario'
+        msg: 'Autentica un usuario',
+        token: token
     });
 }
 
